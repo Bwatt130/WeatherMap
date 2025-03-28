b81a7f0064b0f1f7375f3dcaf5cc5317312dee00
BoodFitch <mbarox23@gmail.com>
2025-03-28 10:40:31 -0400

    default browser test

--- MainWeather.py
+++ MainWeather.py
@@ -22,14 +22,15 @@ def show_map_popup():
     # Create a map object centered at a specific location
     my_map = folium.Map(location=[40.7128, -74.0060], zoom_start=10)  # Example: New York City
 
-    chrome_path = '"C:\Program Files\Google\Chrome\Application\chrome.exe" %s'
+    #chrome_path = '"C:\Program Files\Google\Chrome\Application\chrome.exe" %s'
 
     # Save the map as an HTML file
     map_filepath = "map.html"
     my_map.save(map_filepath)
 
     # Open the HTML file in a web browser using a popup window
-    webbrowser.get(chrome_path).open(f"file://{map_filepath}", new=2)
+    w = webbrowser.get()
+    w.open(f"file://{map_filepath}", new=2)
 
 asyncio.run(getweather())
 m = folium.Map(location=(45.5236, -122.6750))
