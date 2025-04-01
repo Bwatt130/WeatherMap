6c68c4ef586dd92d5a84d54ba31f479e19c36ec2
BoodFitch <mbarox23@gmail.com>
2025-04-01 14:01:41 -0400

    shows highs, lows, daily, and desc of weather

--- MainWeather.py
+++ MainWeather.py
@@ -12,20 +12,37 @@ import os
 async def getweather() -> None:
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
         weather = await client.get('Philadelphia')
+        print(weather.daily_forecasts[0].highest_temperature)
+        print(weather.daily_forecasts[0].lowest_temperature)
     
         # returns the current day's forecast temperature (int)
-        print(weather.temperature)
+        print(str(weather.temperature) + " degrees. Feels like " + str(weather.feels_like) + " degrees")
         
-        # get the weather forecast for a few days
+        # get the weather forecast for 3 days
+        # gets highs and lows of temp
+        highs = []
+        lows = []
         for daily in weather:
-            print(daily)
+            highs.append(daily.highest_temperature)
+            lows.append(daily.lowest_temperature)
+        print(highs)
+        print(lows)
         
-        # hourly forecasts
+        # hourly forecasts every 3 hours
+        today = []
+        desc = []
         for hourly in daily:
-            print(f' --> {hourly!r}')
-def show_map_popup(lat, long):
+            today.append(hourly.temperature)
+            desc.append(hourly.description)
+        print(today)
+        print(desc)
+        
+def createMap(lat, long):
     # Create a map object centered at a specific location
     my_map = folium.Map(location=[lat, long], zoom_start=10)  # Example: New York City
+
+    #Search .csv file for coords
+    #Get city and send to getweather()
 
     df = pd.DataFrame(
     data=[["apple", "oranges"], ["other", "stuff"]], columns=["cats", "dogs"]
@@ -43,34 +60,35 @@ def show_map_popup(lat, long):
     folium.Marker([lat, long], popup=popup).add_to(my_map)
 
     # Save the map as an HTML file
-    map_filepath = "C:\Windows\Temp\map.html"
+    map_filepath = r"C:\Windows\Temp\map.html"
     my_map.save(map_filepath)
 
+    #openWindow(map_filepath)
+
     # Open the HTML file in the user's default web browser using a popup window
-    webbrowser.open(f"file:/{map_filepath}", new=2)
+    #webbrowser.open(f"file:/{map_filepath}", new=2)
+
+def openWindow(filepath):
+    main = tk.Tk()
+    main.config(bg="#E4E2E2")
+    main.title("Main Window")
+    main.geometry("700x400")
+
+    label = tk.Label(master=main, text="Text Box")
+    label.config(bg="#E4E2E2", fg="#000")
+    label.place(x=57, y=76, width=80, height=40)
+
+    text = tk.Text(master=main)
+    text.config(bg="#fff", fg="#000")
+    text.place(x=60, y=154, width=120, height=80)
+
+    frame = HtmlFrame(main)
+    frame.load_file(filepath)
+    print(filepath)
+    frame.pack(fill="both", expand=True)
+
+    main.mainloop()
 
 asyncio.run(getweather())
 m = folium.Map(location=(45.5236, -122.6750))
-#show_map_popup(40.7128, -74.0060)
-
-temp_dir = tempfile.gettempdir()
-html_file_path = os.path.join(temp_dir, "map.html")
-
-main = tk.Tk()
-main.config(bg="#E4E2E2")
-main.title("Main Window")
-main.geometry("700x400")
-
-label = tk.Label(master=main, text="Text Box")
-label.config(bg="#E4E2E2", fg="#000")
-label.place(x=57, y=76, width=80, height=40)
-
-text = tk.Text(master=main)
-text.config(bg="#fff", fg="#000")
-text.place(x=60, y=154, width=120, height=80)
-
-frame = HtmlFrame(main)
-frame.load_website(html_file_path)
-frame.pack(fill="both", expand=True)
-
-main.mainloop()
+createMap(40.7128, -74.0060)
