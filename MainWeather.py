b0077b9ade3d370035bd29a69753f05705f643e4
Bwatt130 <lilwatty130@gmail.com>
2025-04-03 10:22:45 -0400

    Put the HTML table into the pin while the map  opens in a window.

--- MainWeather.py
+++ MainWeather.py
@@ -1,46 +1,86 @@
 import webview
 import tempfile
 import python_weather
+import pandas as pd
 import folium
 import asyncio
+import branca
 import os
 
 # Fetches weather data asynchronously for the given city
-async def fetch_weather(city='Philadelphia'):
+async def fetch_weather(city):
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
         weather = await client.get(city)
-        return {
-            'temperature': weather.temperature,
-            'feels_like': weather.feels_like,
-            'high': weather.daily_forecasts[0].highest_temperature,
-            'low': weather.daily_forecasts[0].lowest_temperature,
-            'description': weather.description
+        print(weather.daily_forecasts[0].highest_temperature)
+        print(weather.daily_forecasts[0].lowest_temperature)
+    
+        # returns the current day's forecast temperature (int)
+        print(str(weather.temperature) + " degrees. Feels like " + str(weather.feels_like) + " degrees")
+        
+        # get the weather forecast for 3 days
+        # gets highs and lows of temp
+        highs = []
+        lows = []
+        dates = []
+        for daily in weather:
+            highs.append(daily.highest_temperature)
+            lows.append(daily.lowest_temperature)
+            dates.append(daily.date)
+        print(highs) #WANT TO RETURN
+        print(lows) #WANT TO RETURN
+        
+        # hourly forecasts every 3 hours
+        today = []
+        desc = []
+        for hourly in daily:
+            today.append(hourly.temperature)
+            desc.append(hourly.description)
+        print(today) #WANT TO RETURN
+        print(desc) #WANT TO RETURN
+        return{
+            "temp": weather.temperature,
+            "feels": weather.feels_like,
+            "highs": highs,
+            "lows": lows,
+            "today": today,
+            "desc": desc,
+            "dates": dates
         }
 
 # Creates the map at given coordinates and puts weather in the popup
-def generate_map(lat, lon, weather_info):
-    map_file = os.path.join(tempfile.gettempdir(), "map.html")  # Path to save the HTML map
+def generate_map(lat, long, weatherInfo):
 
-    # Create weather description for the popup
-    popup_html = f"""
-    <b>Today's Weather:</b><br>
-    Temp: {weather_info['temperature']}°F (Feels like {weather_info['feels_like']}°F)<br>
-    High: {weather_info['high']}°F, Low: {weather_info['low']}°F<br>
-    Condition: {weather_info['description']}
-    """
+    my_file = os.path.join(tempfile.gettempdir(), "map.html")  # Path to save the HTML map
 
     # Create the folium map
-    folium_map = folium.Map(location=[lat, lon], zoom_start=12)
+    my_map = folium.Map(location=[lat, long], zoom_start=12)
+    
+    # Create weather description for the popup
+    df = pd.DataFrame(
+    data=[[str(weatherInfo["temp"]) + " Degrees", "High: " + str(weatherInfo["highs"][1]), "High: " + str(weatherInfo["highs"][2])]
+          , ["Feels like " + str(weatherInfo["feels"]) + " Degrees", "Low: " + str(weatherInfo["lows"][1]),  "Low: " +str(weatherInfo["lows"][2])]
+          , ["High: " + str(weatherInfo["highs"][0]), "tomorrow desc", "aftermorrow desc"]
+          , ["Low: " + str(weatherInfo["lows"][0]),"",""]]
+          , columns=["Today", weatherInfo["dates"][1],weatherInfo["dates"][2]]
+    )
+
+    html = df.to_html(
+        classes="table table-striped table-hover table-responsive",
+        index=False,
+        justify="left"
+    )
+
+    popup = folium.Popup(html)
+
+    iframe = branca.element.IFrame(html=html, width=500, height=300)
+    popup = folium.Popup(iframe, max_width=500)
+
 
     # Add marker with weather popup and tooltip
-    folium.Marker(
-        [lat, lon],
-        popup=popup_html,
-        tooltip="Click for weather"
-    ).add_to(folium_map)
+    folium.Marker([lat, long], popup=popup).add_to(my_map)
 
-    folium_map.save(map_file)  # Save map to file
-    return map_file
+    my_map.save(my_file)  # Save map to file
+    return my_file
 
 # Main function to coordinate map creation and display in webview
 async def main(city, lat, lon):
